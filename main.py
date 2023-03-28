a7d9fec2e7246524129e5ac73cd6d1e57b5ae5a5
AHK <87975316+ht-khtn@users.noreply.github.com>
2023-03-28 17:42:28 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
-from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty, StringProperty, ListProperty
+from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty, StringProperty, ListProperty, DictProperty
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
@@ -15,129 +16,141 @@ from kivy.animation import Animation
 from kivy.clock import time
 from kivy.uix.label import Label
 
-Builder.load_string('''
+Builder.load_file("lichsu.kv")
 
-<Stage1>
-    
-    opacity: 0
-
-<TnlsUI>
-    stage_area: stage_area
-    
-    opacity: 0
-        
-    Button:
-        id: back_btn
-        text: "Back"
-        size: 60,40
-        y: root.height - 50
-        x: 10
-        
-        on_press: root.parent.showUI(root.parent.mainui)
-    
-    GridLayout:
-        id: stage_area
-        center:root.center
-        orientation: "lr-tb"
-        cols: 8
-        size: root.width-50, root.height-200
-        padding: 10
-        spacing: 20
-        
-
-<MainInterface>
-    
-    opacity: 0
-
-    AsyncImage:                                          # Change Main Interface Picture here
-        source: ''
-        center: root.width/2, root.height/2
-        size: 750,750
-
-    Button:
-        id: tnls
-        center_x: root.center_x
-        center_y: root.center_y + 50
-        size: 300,60
-        text: 'Trải nghiệm Lịch sử'
-        font_size: 20
-        
-        on_press: root.parent.showUI(root.parent.tnlsui)
-    
-    Button:
-        id: cckt
-        center_x: root.center_x
-        center_y: root.center_y - 50
-        size: 300,60
-        text: 'Củng cố Kiến thức'
-        font_size: 20
-        
-
-<LoadInterface>
-    opacity: 0
-    
-    AsyncImage:                                          # Change App logo here
-        source: 'app_icon.png'
-        size: 248,260         
-        center: root.width/2, root.height/2 + 75
-        
-    Label:                                               # App Name here
-        text: 'Thắp lửa Tuổi trẻ Việt'
-        text_size: 500,90
-        font_size: 50
-        halign: 'center'
-        color: 0,0,0,1
-        center_x: root.center_x
-        center_y: root.center_y - 150
-
-
-<MainGUI>
-    logo: logo
-    mainui: main
-    tnlsui: tnlsui
-    
-    canvas.before:
-        Color:
-            rgba: 1,1,1,1                                 # Change App Color here
-        Rectangle:
-            size: root.size
-            pos: root.pos
-            
-    LoadInterface:
-        id: logo
-        pos: root.pos
-        size: root.size
-        
-    MainInterface:
-        id: main
-        pos: root.x-1000,root.y
-        size: root.size
-        
-    TnlsUI:
-        id: tnlsui
-        pos: root.x-1000,root.y
-        size: root.size
-    
-    Stage1:
-        id: stage1
-        pos: root.x-1000,root.y
-        size: root.size
-    
-''')
 
 class Stage1(Widget):
+
+    data = {
+        "Cuộc kháng chiến chống quân xâm lược Triệu Đà thất bại dẫn tới hậu quả gì?" : 
+        [
+            "2000 năm Bắc thuộc",
+            "80 năm Pháp thuộc",
+            "2 thập niên Minh thuộc" 
+        ],
+        "Vào đầu thế kỉ XX, nước tư bản chủ nghĩa nào đã ảnh hưởng vào cuộc vận động giải phóng dân tộc Việt Nam là chủ yếu?":
+        [
+            "Nhật",
+            "Nga",
+            "Pháp"
+        ],
+        "Ở giai đoạn đầu thế kỉ XX, các cuộc vận động giải phóng đân tộc Việt Nam đi theo hình thức nào là chủ yếu?":
+        [
+            "vô sản",
+            "dân chủ tư sản",
+            "dân chủ vô sản"
+        ]
+    }
+    true_data = [0,0,1]
+    data_keys = list(data.keys())
+    data_size = len(data_keys)
+    final_result = ""
     
-    pass
+    video = ObjectProperty(None)
+    quiz = ObjectProperty(None)
+    back_btn = ObjectProperty(None)
+    skip_btn  = ObjectProperty(None)
+    question = ObjectProperty(None)
+    answer = ObjectProperty(None)
+    answer1 = ObjectProperty(None)
+    answer2 = ObjectProperty(None)
+    answer3 = ObjectProperty(None)
+    ans_list = ReferenceListProperty(answer1,answer2,answer3)
+    next_btn = ObjectProperty(None)
+    result = ObjectProperty(None)
+    result_status = ObjectProperty(None)
+    score_is = ObjectProperty(None)
+    score = ObjectProperty(None)
+
+    def reset_quiz(self):
+        self.progress = 0
+        self.chosen_ans = 0
+        self.curques = 0
+        self.true_ans_count = 0
+        self.next_btn.text = "Nộp"
+        self.next_btn.disabled = False
+    
+    def reset_video(self):
+        self.video.state = "stop"
+        self.video.state = "play"
+        self.video.opacity = 1
+        self.quiz.opacity = 0
+        self.skip_btn.opacity = 1
+        self.skip_btn.background_normal = "next_btn.png"
+        self.skip_status = 0
+        self.next_btn.disabled = True 
+        self.result.opacity = 0
+        
+    def skip(self):
+        if self.skip_status == 0:
+            self.skip_status = 1
+            self.video.state = "stop"
+            self.video.opacity = 0
+            self.quiz.opacity = 1
+            self.skip_btn.background_normal = "pre_btn.png"
+            self.reset_quiz()
+            self.show_quiz(0)
+        else:
+            self.reset_video()
+        
+    def choose(self,instance,value):
+        print(instance,value)
+    
+    def show_quiz(self, n):
+        self.curques = n
+        self.question.text = f"{self.data_keys[n]}"
+        answer_data = self.data[self.data_keys[n]]
+        for i in range(3):
+            self.ans_list[i].text = answer_data[i]
+            self.ans_list[i].color = (0,0,0,1)
+
+    def on_choose(self,n):
+        self.chosen_ans = n
+
+    def on_click(self):
+        if self.progress % 2 != 0 and self.curques < self.data_size-1:
+            self.next_btn.text = "Nộp"
+            self.show_quiz(self.curques+1)
+        elif self.curques < self.data_size-1:
+            self.next_btn.text = "Câu hỏi tiếp theo"
+            self.show_corect_ans()
+        elif self.progress == -1:
+            self.final_result = f"{self.true_ans_count} / {self.data_size}"
+            self.show_result()
+        else:
+            self.next_btn.text = "Kết thúc bài làm"
+            self.show_corect_ans()
+            self.progress = -2
+        self.progress += 1
+    
+    def show_corect_ans(self):
+        true_ans = self.true_data[self.curques]
+        self.ans_list[true_ans].color = (0,1,0,1)
+        if self.chosen_ans == true_ans:
+            self.true_ans_count += 1
+        else:
+            self.ans_list[self.chosen_ans].color = (1,0,0,1)
+
+    def show_result(self):
+        self.quiz.opacity = 0
+        self.next_btn.disabled = True
+        self.score.text = self.final_result
+        self.result.opacity = 1
 
 
 class TnlsUI(Widget):
     
     stage_area = ObjectProperty(None)
     
+    opened_stage = NumericProperty(1)
+    
     def create(self):
         for i in range(30):
-            btn = Button(text = f"{i+1}")
-            btn.bind(on_press = lambda x: self.parent.goto_stage(i))
+            if i < self.opened_stage:
+                btn = Button(text = f"{i+1}", background_color = (1,1,1,1))
+            else:
+                btn = Button(text = f"{i+1}", background_color = (1,1,1,0.5))
+            btn.bind(on_press = self.parent.goto_stage)
             self.stage_area.add_widget(btn)
     
 
@@ -156,7 +169,6 @@ class MainGUI(Widget):
     logo = ObjectProperty(None)
     mainui = ObjectProperty(None)
     tnlsui = ObjectProperty(None)
-    
     stage1 = ObjectProperty(None)
     
     stage = ReferenceListProperty(stage1)
@@ -182,24 +194,30 @@ class MainGUI(Widget):
     def on_complete(self,animation,widget):
         if widget == self.logo:
             self.showUI(self.mainui)
-        print(widget)
             
     def showUI(self,ui):
         if self.curui:
             self.fadeout.start(self.curui)
             self.curui.x -= 1000
+            if self.curui in self.stage:
+                self.curui.video.state = "stop"
+        if ui in self.stage:
+            ui.reset_video()
         ui.x += 1000
         self.fadein.start(ui)
         self.curui = ui
     
-    def goto_stage(self,stage):
-        print(stage)
-        self.showUI(self.stage[stage])
+    def goto_stage(self,obj):
+        if obj.background_color == [1,1,1,1]:
+            self.showUI(self.stage[int(obj.text)-1])
     
 
 class LichSuApp(App):
     
+    title = "Lịch Sử"
+    
     def build(self):
+        self.icon = "app_icon.png"
         return MainGUI()
         
 if __name__ == '__main__':
